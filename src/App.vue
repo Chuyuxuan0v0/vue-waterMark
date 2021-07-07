9c15c548cedb6918dd419b676f2b72b93f03972d
Chuyuxuan <1647364613@qq.com>
2021-07-07 11:36:57 +0800

    update

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import { removeWatermark, setWaterMark } from './common/water_mark'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  mounted() {
+    setWaterMark('liergou', '李二狗');
+},
+destroyed() {
+    removeWatermark();
+},
+}
+</script>
+
+<style>
+
+</style>
